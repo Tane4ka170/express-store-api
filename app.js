9cde59c1badf70ec384862f8530db0ceac851d11
Tane4ka170 <tane4ka170@gmail.com>
2024-03-19 02:34:33 +0200

    started in app

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("04 Store API");
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Store API</h1><a href='/api/v1/products'>products route</a>");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
